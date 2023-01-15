f8155541e24627ab77eb2d825ab1708faf669c76
luch-o <luiscarrodba97@gmail.com>
2023-01-15 18:29:20 -0500

    add store function and requirements to test locally

---
+++ w1-backend/functions/store.py
@@ -0,0 +1,11 @@
+import boto3, uuid
+
+client = boto3.resource('dynamodb')
+table = client.Table("orders")
+
+def lambda_handler(event, context):
+    for record in event['Records']:
+        print("test")
+        payload = record["body"]
+        print(str(payload))
+        table.put_item(Item= {'orderID': str(uuid.uuid4()),'order':  payload})
